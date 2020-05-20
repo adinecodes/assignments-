0424421828637dea727863b8a9dee67ba8e5ba02
Adine Mateo <adinemateo1@gmail.com>
2020-05-19 17:19:14 -0700

    Loop Olympics

--- moms-shopping-list-start-master/index.js
+++ moms-shopping-list-start-master/index.js
@@ -4,6 +4,7 @@ let shoppingList = []
 
 //Create a function that creates a new list item and displays it on the dom
 function addItem (item) {
+  
   //Create Phase
   const newItem = document.createElement("li")  
   const deleteButton = document.createElement("button")
@@ -16,12 +17,12 @@ function addItem (item) {
   list.appendChild(newItem)
   newItem.appendChild(deleteButton)
 
+  shoppingList.push(item)
 }
 
 //Add an event listener to the form ( submit)
 form.addEventListener("submit", function(event){
   event.preventDefault()
-  shoppingList.push(form.title.value)
   const submittedItem = form.title.value
   form.title.value= ""
   addItem(submittedItem)
@@ -30,17 +31,21 @@ form.addEventListener("submit", function(event){
 //Create a function that removes all the items from the list
 function clearList() {
   //Iterate through each item of the list and remove it
-  for(let i = 0; i < list.childNodes.length; i++){
+  while(list.childNodes[0]){
     //Remove Child Method
     list.removeChild(list.childNodes[0])
   }
+  shoppingList = []
+ 
+  
 }
 
 
 //Create a function that adds all items to the list
-function fillList(){
-  for(let i = 0; i < shoppingList.lenght; i++){
-    addItem(shoppingList[i])
+function fillList(arr){
+ 
+  for(let i = 0; i < arr.length; i++){
+    addItem(arr[i])
   }
 }
 
@@ -53,10 +58,9 @@ function handleDelete(item){
       updatedList.push(shoppingList[i])
     }
   }
-  shoppingList = updatedList
-
+ 
   clearList()
-  fillList()
+  fillList(updatedList)
 }
 
 
