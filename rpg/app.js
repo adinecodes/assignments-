e077e1d5e1ff2b6c649c8fa436c6a1255d73917a
Adine Mateo <adinemateo1@gmail.com>
2020-06-02 21:55:25 -0700

    rpg

--- rpg/app.js
+++ rpg/app.js
@@ -15,11 +15,42 @@ if (action === "w") {
 
 function walk(){
   console.log("you are walking!")
-   Math.floor(Math.random)
-   let enemy = Math.floor(Math.random() * 3) + 1 ;
-   console.log ('You have encountered a killer Spider')
+   const randomNum = Math.floor(Math.random() * 3) + 1 ;
+   if (randomNum === 3) {
+
+     const enemyArray = ["killer spider","flying monkey","taco the dog"]
+     const enemyName = enemyArray[Math.floor(Math.random() * enemyArray.length)]
+     const enemyHp = Math.floor(Math.random() * 15) + 15 ;
+     const randomEnemy = {name: enemyName, hp: enemyHp}
+     console.log ('You have encountered', randomEnemy.name)
+     const action =readline.keyIn ("What would you like to do [a] attack,[r] run?", {limit: "ar"})
+     if (action === "a"){
+
+       fightSequence(randomEnemy)
+     }else{
+       if(Math.floor(Math.random() * 2) + 1 === 1) {
+         console.log("you can't escape the enemy")
+         fightSequence(randomEnemy)
+       }else {
+
+         console.log("I got away")
+       }
+     }
+   } else {
+     console.log("Lucky me I didn't see any enemies!")
+   }
   
 }
 
+function fightSequence(enemy){
+  console.log("I'm fighting " + enemy.name)
+  const myAttack = Math.floor(Math.random() * 20) + 10 ;
+  const enemyAttack = Math.floor(Math.random() * 10) + 5 ;
+  enemy.hp = enemy.hp - myAttack
+  console.log("you hit the enemy, the enemy has " + enemy.hp + "hp")
+  if (enemy.hp > 0) {
+    fightSequence(enemy)
+  }
+}
 
 
